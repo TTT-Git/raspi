ca5b42c322b5a67ac43ed17698e30329b852c425
TTT-Git <tsune33@gmail.com>
2021-09-11 22:51:15 +0900

    add function control from python

--- settings.py
+++ settings.py
@@ -9,7 +9,8 @@ use_co2 = int(conf['DEVICE']['co2'])
 use_pulseio_dht22 = conf.getboolean('DEVICE', 'use_pulseio_dht22')
 
 gpio_dht22 = int(conf['GPIO_NUM']['dht22'])
-gpio_irreceiver = int(conf['GPIO_NUM']['irreceiver'])
+gpio_irreceiver = int(conf['GPIO_NUM']['ir_receiver'])
+gpio_irled = int(conf['GPIO_NUM']['ir_led'])
 
 out_file_name = conf['FILE']['out_file_name']
 
