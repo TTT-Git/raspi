95510d8a6cefa015b6edea17809ffb43d3efe486
TTT-Git <tsune33@gmail.com>
2022-01-19 22:10:11 +0900

    add scp function

--- settings.py
+++ settings.py
@@ -28,3 +28,7 @@ gpio_irled = int(conf['GPIO_NUM']['ir_led'])
 
 out_file_name = conf['FILE']['out_file_name']
 
+
+host = str(conf['SSH']['host'])
+user = str(conf['SSH']['user'])
+key_file = str(conf['SSH']['key_file'])
---
+++ test_veiws.py
@@ -0,0 +1,6 @@
+
+from views import views
+
+r = views.Raspi('raspi0-1')
+r.copy_data(file_name='result20220115.csv')
+
