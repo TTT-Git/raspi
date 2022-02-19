75eb763c15da6d7fb063e6578deb5c920b4561b6
TTT-Git <tsune33@gmail.com>
2022-02-19 15:12:19 +0900

    add function of save to sql and html view

---
+++ controllers/webserver.py
@@ -0,0 +1,72 @@
+from datetime import datetime
+from datetime import timedelta
+from flask import Flask
+from flask import jsonify
+from flask import render_template
+from flask import request
+import logging
+
+from models.df_temp_humid import DataFrameTempHumid
+
+import settings
+
+logger = logging.getLogger(__name__)
+
+app = Flask(__name__, template_folder='../views')
+
+
+@app.teardown_appcontext
+def remove_session(ex=None):
+    from models.base import  Session
+    Session.remove()
+
+
+@app.route('/')
+def index():
+    # hostname = settings.system_name
+    # device_num = 0
+    # df = DataFrameTempHumid(hostname, device_num)
+    # term_hour = 24
+    # now = datetime.utcnow()
+    # time = now - timedelta(hours=term_hour)
+    # df.set_data_after_time(time)
+    
+    # temp_humid_data = df.value['temp_humid']
+    # print(temp_humid_data)
+    # return render_template('./test.html', temp_humid_data=temp_humid_data)
+    return render_template('./jquery.html')
+
+@app.route('/api/candle/', methods=['GET'])
+def api_make_handler():
+    hostname = request.args.get('hostname')
+    if not hostname:
+        return jsonify({'error': 'No hostname params'}) ,400
+    
+    term_hour_str = request.args.get('term_hour')
+    term_hour = 24
+    if term_hour_str:
+        term_hour = int(term_hour_str)
+
+    if term_hour < 0 or term_hour > 720:
+        term_hour = 720
+
+    device_num_str = request.args.get('device_num')
+    if device_num_str:
+        device_num = int(device_num_str)
+    else:
+        device_num = 0
+    
+    df = DataFrameTempHumid(hostname, device_num)
+
+    now = datetime.now()
+    time = now - timedelta(hours=term_hour)
+    df.set_data_after_time(time)
+
+    return jsonify(df.value), 200
+
+
+
+
+def start():
+    app.run(host='0.0.0.0', port=settings.web_port, threaded=True)
+
--- models/data_collector.py
+++ models/data_collector.py
@@ -1,11 +1,13 @@
 import json
-import os
-import subprocess
+import logging
 
 import paramiko
+import socket
 
 import main_return_onece
 import settings
+
+logger = logging.getLogger(__name__)
 
 class Raspi(object):
     def __init__(self, ssh_num=0, remote=True) -> None:
@@ -21,9 +23,27 @@ class Raspi(object):
                 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
                 # ssh接続する
-                ssh.connect(hostname=settings.ssh[self.ssh_num]['host'], 
-                    username=settings.ssh[self.ssh_num]['user'],
-                    key_filename=settings.ssh[self.ssh_num]['key_file'])
+                try:
+                    ssh.connect(hostname=settings.ssh[self.ssh_num]['host'], 
+                        username=settings.ssh[self.ssh_num]['user'],
+                        key_filename=settings.ssh[self.ssh_num]['key_file'])
+                except socket.gaierror as e:
+                    logger.error({
+                            'action': 'ssh connect',
+                            'status': 'socket.gaierror',
+                            'message': f'error message: {e}',
+                            'data': f"hostname={settings.ssh[self.ssh_num]['host']}, username={settings.ssh[self.ssh_num]['user']}, key_filename={settings.ssh[self.ssh_num]['key_file']}"
+                        })
+                    return {}
+                except ValueError as e:
+                    logger.error({
+                            'action': 'ssh connect',
+                            'status': 'ValueError',
+                            'message': f'error message: {e}',
+                            'data': f"hostname={settings.ssh[self.ssh_num]['host']}, username={settings.ssh[self.ssh_num]['user']}, key_filename={settings.ssh[self.ssh_num]['key_file']}"
+                        })
+                    return {}
+
 
                 # サーバー上で実行するコマンドを設定
                 CMD = 'cd ' + py_file_path + ' ; python3 ' + py_file_name
@@ -34,9 +54,16 @@ class Raspi(object):
                 for line in stdout:
                     try:
                         self.result_dict = json.loads(line)
+                        for device_num in range(2):
+                            self.result_dict[device_num] = self.result_dict.pop(str(device_num))
                         self.result_dict['hostname'] = settings.ssh[self.ssh_num]['host']
                     except json.JSONDecodeError as e:
-                        print(e, line)
+                        logger.error({
+                            'action': 'json decode',
+                            'status': 'json.JSONDecodeError',
+                            'message': f'error message {e}',
+                            'data': f'line={line}'
+                        })
         
         else:
             self.result_dict = main_return_onece.main_func()
